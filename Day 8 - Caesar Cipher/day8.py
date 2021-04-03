317700a0aff9f03d3d49c07481006649fad9c664
Johnsons Sangah <ujaama.cbr@gmail.com>
2021-04-03 23:53:29 +1100

    files moved to correct folders

---
+++ Day 8 - Caesar Cipher/day8.py
@@ -0,0 +1,15 @@
+# prime = 2 % 2
+# prime1 = 2 % 1
+
+
+# print(prime, prime1)
+
+n = int(input("Check this number: "))
+
+def prime_checker(n):
+    if (n % n == 0 and n % 1 == 0):
+        print("It's a prime number")
+    else:
+        print("It's not a prime number")
+
+prime_checker(n)
